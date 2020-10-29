a16a45c6e000acf770f954b57a0e40579f87da46
jdsdc <jd@sdc-automation.se>
2020-10-29 10:10:06 +0100

    Add 16000 pumpnodes for test and default value set to 90

--- build/scripts/dslink-java-v2-simulator.bat
+++ build/scripts/dslink-java-v2-simulator.bat
@@ -1,23 +1,7 @@
-@rem
-@rem Copyright 2015 the original author or authors.
-@rem
-@rem Licensed under the Apache License, Version 2.0 (the "License");
-@rem you may not use this file except in compliance with the License.
-@rem You may obtain a copy of the License at
-@rem
-@rem      https://www.apache.org/licenses/LICENSE-2.0
-@rem
-@rem Unless required by applicable law or agreed to in writing, software
-@rem distributed under the License is distributed on an "AS IS" BASIS,
-@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@rem See the License for the specific language governing permissions and
-@rem limitations under the License.
-@rem
-
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
-@rem  Gradle startup script for Windows
+@rem  dslink-java-v2-simulator startup script for Windows
 @rem
 @rem ##########################################################################
 
@@ -27,10 +11,10 @@ if "%OS%"=="Windows_NT" setlocal
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
+set APP_HOME=%DIRNAME%..
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+@rem Add default JVM options here. You can also use JAVA_OPTS and DSLINK_JAVA_V2_SIMULATOR_OPTS to pass JVM options to this script.
+set DEFAULT_JVM_OPTS=
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
@@ -79,19 +63,19 @@ set CMD_LINE_ARGS=%*
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+set CLASSPATH=%APP_HOME%\lib\dslink-java-v2-simulator-1.0.0.jar;%APP_HOME%\lib\dslink-v2-websocket-0.75.2.jar;%APP_HOME%\lib\dslink-v2-0.75.2.jar;%APP_HOME%\lib\tyrus-standalone-client-1.16.jar;%APP_HOME%\lib\dslink-v2-api-0.75.2.jar
 
-@rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
+@rem Execute dslink-java-v2-simulator
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %DSLINK_JAVA_V2_SIMULATOR_OPTS%  -classpath "%CLASSPATH%" org.iot.dsa.dslink.DSLink %CMD_LINE_ARGS%
 
 :end
 @rem End local scope for the variables with windows NT shell
 if "%ERRORLEVEL%"=="0" goto mainEnd
 
 :fail
-rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
+rem Set variable DSLINK_JAVA_V2_SIMULATOR_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
-if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
+if  not "" == "%DSLINK_JAVA_V2_SIMULATOR_EXIT_CONSOLE%" exit 1
 exit /b 1
 
 :mainEnd
